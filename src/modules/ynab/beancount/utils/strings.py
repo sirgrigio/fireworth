d7dbc530f52c4b3b427c8015ed66d737b2858962
Matteo Zaccagnino <matteozaccagnino@mail.com>
2025-08-24 10:23:22 +0200

    added trimming

--- src/modules/ynab/beancount/utils/strings.py
+++ src/modules/ynab/beancount/utils/strings.py
@@ -1,7 +1,7 @@
 import re
 
 def lowerdashed(value: str) -> str:
-    return re.sub(r'\s+', '-', value.lower().replace('&', 'and').replace(':', ' ')) if value else value
+    return re.sub(r'\s+', '-', value.lower().strip().replace('&', 'and').replace(':', ' ')) if value else value
 
 def camelcased(value: str) -> str:
-    return ' '.join([w.title() if w.islower() else w for w in value.split()]) if value else value
+    return ' '.join([w.title() if w.islower() else w for w in value.strip().split()]) if value else value
